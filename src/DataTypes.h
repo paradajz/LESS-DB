6a32c7a9b9c733674fe5be6cc96602cb7205aaa7
paradajz <2544094+paradajz@users.noreply.github.com>
2018-02-22 10:44:57 +0100

    update licence headers

--- src/DataTypes.h
+++ src/DataTypes.h
@@ -1,5 +1,5 @@
 /*
-    Copyright 2017 Igor Petroviæ
+    Copyright 2017-2018 Igor Petrovic
 
     Permission is hereby granted, free of charge, to any person obtaining
     a copy of this software and associated documentation files (the "Software"),
